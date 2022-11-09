6f768aa275f2ad35f2bdd3e4c72653640f780dc4
HunMyeong <102712296+HunMyeong@users.noreply.github.com>
2022-11-09 16:33:06 +0900

    22.11.09
    로컬스토리지

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CALSSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CALSSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
     }
-loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CALSSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // form을 보이게.
+    loginForm.classList.remove(HIDDEN_CALSSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
